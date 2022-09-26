c9eb50fbc661fb3bf743041bfd33224ac529a703
김세현 <dnjfdid14@naver.com>
2022-09-26 22:32:11 +0900

    NaN

---
+++ Pandas/10. 결측치.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np
+df = pd.read_csv('people.csv')
+
+print(df.fillna('없음')) #빈칸을 없음 이라고 채움
+
+df['학교'] = np.nan # 학교 데이터 전체를 NaN으로 만듬
+print(df)
+
+print(df.dropna()) # 전체 데이터 중 NaN을 포함하는 데이터 삭제
