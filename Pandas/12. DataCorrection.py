4947295ff61f224bbc944c72771be08c727c27f0
김세현 <dnjfdid14@naver.com>
2022-09-29 16:14:00 +0900

    correction

--- Pandas/12. DataCorrection.py
+++ Pandas/12. DataCorrection.py
@@ -6,3 +6,11 @@ print(df)
 
 df['학교'] = df['학교'] + '등학교' # 학교에 등학교 단어를 붙임
 print(df)
+
+# Column 추가
+df['총합점수'] = df['국어'] + df['영어'] + df['수학'] + df['과학'] + df['사회']
+
+df['결과'] = 'Fall'
+df.loc[df['총합점수'] > 400, '결과'] = 'Pass'
+
+print(df)
