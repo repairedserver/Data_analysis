48acaf82a39b6e06272c62b35e0ab34eb2e8b18e
김세현 <dnjfdid14@naver.com>
2022-10-22 10:17:27 +0900

    basic

--- Matplotlib/1. 그래프 기본.py
+++ Matplotlib/1. 그래프 기본.py
@@ -1,6 +1,13 @@
 import matplotlib.pyplot as plt
+import matplotlib as mat
+mat.rcParams['font.family'] = 'AppleGothic'
+mat.rcParams['font.size'] = 15
+mat.rcParams['axes.unicode_minus'] = False # 한글폰트 사용시 글자깨짐 방지
 x = [1, 2, 3]
 y = [2, 4, 8]
 plt.plot(x, y)
 plt.title('Line Graph')
+plt.show()
+
+plt.plot([-1, 0, 1], [-5, -1, -2])
 plt.show()
