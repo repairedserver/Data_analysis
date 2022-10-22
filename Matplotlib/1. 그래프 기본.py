1de0ebb6f54615cebdb97460433f4ff60a75b0a5
김세현 <dnjfdid14@naver.com>
2022-10-23 08:59:17 +0900

    basic

--- Matplotlib/1. 그래프 기본.py
+++ Matplotlib/1. 그래프 기본.py
@@ -2,10 +2,7 @@ import matplotlib.pyplot as plt
 import matplotlib as mat
 mat.rcParams['font.family'] = 'AppleGothic'
 mat.rcParams['font.size'] = 15
-<<<<<<< HEAD
 mat.rcParams['axes.unicode_minus'] = False # 한글폰트 사용시 글자깨짐 방지
-=======
->>>>>>> main
 x = [1, 2, 3]
 y = [2, 4, 8]
 plt.plot(x, y)
