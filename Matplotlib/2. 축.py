2d957315f298cc4af0206ea7d625378aea3fc84d
김세현 <dnjfdid14@naver.com>
2022-10-23 09:12:02 +0900

    축

--- Matplotlib/2. 축.py
+++ Matplotlib/2. 축.py
@@ -5,3 +5,9 @@ mat.rcParams['font.size'] = 15
 mat.rcParams['axes.unicode_minus'] = False
 x = [1, 2, 3]
 y = [2, 4, 8]
+plt.plot(x, y)
+plt.xlabel('X축', color='red', loc='right')
+plt.ylabel('Y축', color='blue', loc='top')
+plt.title('꺾은선 그래프', fontdict={'size' : 20})
+plt.show()
+
