b8f5f2e098959135b02aba2020d5039dc77afaf4
김세현 <dnjfdid14@naver.com>
2022-10-25 21:52:15 +0900

    스타일

--- Matplotlib/4. 스타일.py
+++ Matplotlib/4. 스타일.py
@@ -5,8 +5,17 @@ mat.rcParams['font.size'] = 15
 mat.rcParams['axes.unicode_minus'] = False
 x = [1, 2, 3]
 y = [2, 4, 8]
-plt.plot(x, y, linewidth=5)
-plt.show()
+# plt.plot(x, y, linewidth=5)
+# plt.show()
 
-plt.plot(x, y, marker='o')
-plt.show()
+# plt.plot(x, y, marker='o')
+# plt.show()
+
+# plt.plot(x, y, marker='o', linestyle='None')
+# plt.show()
+
+# plt.plot(x, y, marker='v', markersize=10)
+# plt.show()
+
+# plt.plot(x, y, marker='o', markersize=15, markeredgecolor='red', markerfacecolor='yellow')
+# plt.show()
