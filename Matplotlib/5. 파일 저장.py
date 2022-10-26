b270c904f6a5e653342851c8f0ffdb6c76af953d
김세현 <dnjfdid14@naver.com>
2022-10-26 21:36:35 +0900

    저장

---
+++ Matplotlib/5. 파일 저장.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt
+import matplotlib as mat
+mat.rcParams['font.family'] = 'AppleGothic'
+mat.rcParams['font.size'] = 15
+mat.rcParams['axes.unicode_minus'] = False
+x = [1, 2, 3]
+y = [2, 4, 8]
+plt.plot(x, y)
