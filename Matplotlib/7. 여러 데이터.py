4808a4c98178f49648c491015a03500921b7683c
김세현 <dnjfdid14@naver.com>
2022-10-31 21:39:58 +0900

    여러데이터

---
+++ Matplotlib/7. 여러 데이터.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+import matplotlib as mat
+mat.rcParams['font.family'] = 'AppleGothic'
+mat.rcParams['font.size'] = 15
+mat.rcParams['axes.unicode_minus'] = False
+x = [1, 2, 3]
+y = [2, 4, 8]
+
+#백신접종인구
+days = [1, 2, 3]
+az = [2, 6, 10]
+pfizer = [5, 1, 3]
