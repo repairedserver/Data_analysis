bc2f3d0778c5addabf41a87b8136143e9b1b1ad8
김세현 <dnjfdid14@naver.com>
2022-11-01 19:25:18 +0900

    막대그래프

--- Matplotlib/9. 막대그래프2.py
+++ Matplotlib/9. 막대그래프2.py
@@ -4,3 +4,17 @@ mat.rcParams['font.family'] = 'AppleGothic'
 mat.rcParams['font.size'] = 15
 mat.rcParams['axes.unicode_minus'] = False
 
+labels = ["김하재", "유현세", "이담현"]
+values = [190, 187, 210]
+
+plt.barh(labels, values)
+plt.xlim(180, 220)
+barh = plt.barh(labels, values)
+barh[0].set_hatch('/')
+barh[1].set_hatch('x')
+barh[2 ].set_hatch('..')
+plt.show()
+
+for idx, rect in enumerate(barh):
+    plt.text(idx, rect.get_height(), values[idx])
+plt.show()
