3ea8041f6f2614de0e994298280f6886e74d0990
김세현 <dnjfdid14@naver.com>
2022-11-01 17:32:19 +0900

    막대그래프

--- Matplotlib/8. 막대그래프1.py
+++ Matplotlib/8. 막대그래프1.py
@@ -9,3 +9,15 @@ values = [190, 187, 210]
 colors = ['r', 'g', 'b']
 plt.bar(labels, values, color=colors)
 plt.show()
+
+labels = ["김하재", "유현세", "이담현"]
+values = [190, 187, 210]
+plt.bar(labels, values, width=0.5)
+plt.ylim(150, 220)
+plt.show()
+
+labels = ["김하재", "유현세", "이담현"]
+values = [190, 187, 210]
+plt.bar(labels, values, width=0.5)
+plt.xticks(rotation=45)
+plt.show()
