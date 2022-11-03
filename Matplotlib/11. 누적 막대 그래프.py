e5fb942aada352e8ad5b2a462fc2f867d6bc3622
김세현 <dnjfdid14@naver.com>
2022-11-03 21:52:43 +0900

    누적 막대 그래프

--- Matplotlib/11. 누적 막대 그래프.py
+++ Matplotlib/11. 누적 막대 그래프.py
@@ -6,3 +6,9 @@ mat.rcParams['font.size'] = 15
 mat.rcParams['axes.unicode_minus'] = False
 
 df = pd.read_csv('score.csv')
+plt.bar(df['이름'], df['국어'])
+plt.bar(df['이름'], df['영어'], bottom=df['국어'])
+plt.bar(df['이름'], df['수학'], bottom=df['국어']+df['영어'])
+plt.xticks(rotation=60)
+plt.legend()
+plt.show()
