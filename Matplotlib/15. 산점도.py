1358a7acce40beb4089f4920388384230fde2ba4
김세현 <dnjfdid14@naver.com>
2022-11-04 16:44:47 +0900

    산점도

--- Matplotlib/15. 산점도.py
+++ Matplotlib/15. 산점도.py
@@ -6,9 +6,12 @@ mat.rcParams['font.family'] = 'AppleGothic'
 mat.rcParams['font.size'] = 15
 mat.rcParams['axes.unicode_minus'] = False
 
+sizes = np.random.rand(8) * 1000
+
 df = pd.read_csv('score.csv')
 df['학년'] = [3, 3, 2, 1, 1, 3, 2, 2]
-plt.scatter(df['영어'], df['수학'])
+plt.scatter(df['영어'], df['수학'], s=sizes, c=df['학년'], cmap='viridis', alpha=0.3)
+plt.figure(figsize=(10, 10))
 plt.xlabel('영어 점수')
 plt.ylabel('수학 점수')
 plt.show()
