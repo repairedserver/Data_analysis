51e22c229d5130b7a79e1c40432057c5c05062ad
김세현 <dnjfdid14@naver.com>
2022-11-04 15:31:32 +0900

    다중 막대 그래프

--- Matplotlib/12. 다중 막대 그래프.py
+++ Matplotlib/12. 다중 막대 그래프.py
@@ -1,9 +1,15 @@
 import matplotlib.pyplot as plt
 import matplotlib as mat
 import pandas as pd
-
+import numpy as np
 mat.rcParams['font.family'] = 'AppleGothic'
 mat.rcParams['font.size'] = 15
 mat.rcParams['axes.unicode_minus'] = False
 
 df = pd.read_csv('score.csv')
+arr = np.arange(5)
+N = df.shape[0]
+w = 0.25
+index = np.arange(N)
+plt.bar(index-w, df['국어'])
+plt.show()
