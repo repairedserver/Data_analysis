8156faff21acf5a33e22256815192a108bd7c464
김세현 <dnjfdid14@naver.com>
2022-11-04 16:11:21 +0900

    원그래프2

--- Matplotlib/14. 원 그래프2.py
+++ Matplotlib/14. 원 그래프2.py
@@ -6,6 +6,9 @@ mat.rcParams['font.family'] = 'AppleGothic'
 mat.rcParams['font.size'] = 15
 mat.rcParams['axes.unicode_minus'] = False
 
+def custom_autopct(pct):
+    return '{:.0f}%'.format(pct) if pct >= 10 else ''
+
 values = [30, 25, 6, 10, 22, 40]
 labels = ['C', 'Python', 'C#', 'JAVA', 'C++', 'ECT']
 # colors = ['r', 'g', 'b', 'y', 'm', 'c']
@@ -13,6 +16,6 @@ colors = ['#ffadad', '#ffd6a5', '#fdffb6', '#caffbf', '#9bf6ff', '#a0c4ff',]
 explode = [0.05] * 6
 plt.pie(values, labels=labels, autopct='%.1f', colors=colors, explode=explode)
 plt.show()
-wedgeprops = {'width': 0.25}
-plt.pie(values, labels=labels, autopct='%.1f%%', colors=colors, explode=explode, wedgeprops=wedgeprops)
+wedgeprops = {'width': 0.25, 'edgecolor': 'w', 'linewidth': 3}
+plt.pie(values, labels=labels, autopct=custom_autopct, colors=colors, explode=explode, wedgeprops=wedgeprops)
 plt.show()
