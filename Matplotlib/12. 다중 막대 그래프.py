91777689ffc5bad1bcb195594f7a2ec098e042db
김세현 <dnjfdid14@naver.com>
2022-11-04 15:33:48 +0900

    다중 막대 그래프

--- Matplotlib/12. 다중 막대 그래프.py
+++ Matplotlib/12. 다중 막대 그래프.py
@@ -11,5 +11,7 @@ arr = np.arange(5)
 N = df.shape[0]
 w = 0.25
 index = np.arange(N)
-plt.bar(index-w, df['국어'])
+plt.bar(index-w, df['국어'], width=w)
+plt.bar(index, df['영어'], width=w)
+plt.bar(index+w, df['수학'], width=w)
 plt.show()
