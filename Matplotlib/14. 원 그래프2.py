825a0660310b08e67f8a0e20adc061182e1b6b89
김세현 <dnjfdid14@naver.com>
2022-11-04 16:02:43 +0900

    원그래프2

--- Matplotlib/14. 원 그래프2.py
+++ Matplotlib/14. 원 그래프2.py
@@ -8,4 +8,11 @@ mat.rcParams['axes.unicode_minus'] = False
 
 values = [30, 25, 6, 10, 22, 40]
 labels = ['C', 'Python', 'C#', 'JAVA', 'C++', 'ECT']
-plt.pie(values, labels=labels, autopct='%.1f', startangle=90, counterclock=False)
+# colors = ['r', 'g', 'b', 'y', 'm', 'c']
+colors = ['#ffadad', '#ffd6a5', '#fdffb6', '#caffbf', '#9bf6ff', '#a0c4ff',]
+explode = [0.05] * 6
+plt.pie(values, labels=labels, autopct='%.1f', colors=colors, explode=explode)
+plt.show()
+wedgeprops = {'width': 0.25}
+plt.pie(values, labels=labels, autopct='%.1f%%', colors=colors, explode=explode, wedgeprops=wedgeprops)
+plt.show()
